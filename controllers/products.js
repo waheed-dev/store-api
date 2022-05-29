cf19e07bd77f4ecf2f2b940bbc0fc85b47fd3496
abdul waheed <a.waheed3738@gmail.com>
2022-05-29 23:54:06 +0500

    added sorting

--- controllers/products.js
+++ controllers/products.js
@@ -5,10 +5,36 @@ const tasks = await products.create(req.body)
 res.status(200).json({tasks})
 }
 const getAllProductsStatic = async (req,res) => {
-    const tasks = await products.find({featured : true})
-    res.status(200).json({tasks,nbOfHita : tasks.length})
+    const items = await products.find({}).sort('-price')
+    res.status(200).json({items,nbOfHita : items.length})
     }
 const getAllProducts = async (req,res) => {
-    res.status(200).json({msg : 'all products'})
+    const {featured,company,name,sort,fields} = req.query
+    const queryObject = {}
+    if (featured) {
+        queryObject.featured = featured === 'true' ? true : false
+    }
+    if (company) {
+        queryObject.company = company
+    }
+    if (name) {
+
+        queryObject.name =  {$regex : name,$options : 'i'}
+    }
+    console.log(queryObject);
+    let result =  products.find(queryObject)
+    if (fields) {
+        const selectedList = fields.split(',').join(' ')
+        result.select(selectedList)
+    }
+    if (sort) {
+        const sorted = sort.split(',').join(' ')
+        result.sort(sorted)
+    }
+    else {
+        result.sort('createdAt')
+    }
+    const items = await result
+    res.status(200).json({items,nbOfHita : items.length})
     }
 module.exports = {getAllProductsStatic,getAllProducts,CreateProductsStatic}
